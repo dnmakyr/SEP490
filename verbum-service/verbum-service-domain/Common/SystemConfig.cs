68b9cbaebb0e98df57f3835c700d7c8343d1bc6e
Le Dai Nguyen <dnsc1503@gmail.com>
2025-02-16 01:47:32 +0700

    resolve secrets in backend

--- verbum-service/verbum-service-domain/Common/SystemConfig.cs
+++ verbum-service/verbum-service-domain/Common/SystemConfig.cs
@@ -8,11 +8,6 @@ namespace verbum_service_domain.Common
 {
     public static class SystemConfig
     {
-        public const string CONNECTION_STRING = "";
-        public const int ACCESS_TOKEN_LIFE = 5; //hour
-        public const int REFRESH_TOKEN_LIFE = 14; //days
-        public const string DOMAIN = "http://localhost:8000/api/auth";
-        public const string BE_DOMAIN = "http://localhost:8000/api";
-        public const string FE_DOMAIN = "http://localhost:3000";
+        
     }
 }
